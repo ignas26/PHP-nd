a251041d323481d58813a179f77b8f783ff01e0c
ignas26 <ignas.zlatkus@gmail.com>
2018-02-22 00:50:10 +0200

    php nd 09 finished

--- p09/index.php
+++ p09/index.php
@@ -1,59 +1,5 @@
 <?php
 
-class Post
-{
- private const MAX_LENGHT = 100;
-private $title = null;
-private $content = null;
-private $author = null;
-   
-    
-    //priskiria atitinkamu klases lauku reiksme
-      public function setTitle ($title) {
-    $this->title = $title;
-    }
-     //grazina atitinkamu klases lauku reiksme
-    public function getTitle(){
-         return $this->title;
-    }
-     //priskiria atitinkamu klases lauku reiksme
-    public static function setContent($content){
-        if(strlen($content) > self::MAX_LENGHT) {
-            echo "Max length is: ".self::MAX_LENGHT."<br>";
-        };
-    }
-     //grazina atitinkamu klases lauku reiksme
-       public static function getContent(){ 
-        return self::MAX_LENGHT;
-       }
-     //priskiria atitinkamu klases lauku reiksme
-    public function setAuthor(Person $person){
-        $this->author = $author;
-    }
-     //grazina atitinkamu klases lauku reiksme
-        public function getAuthor(){
-         return $this->author;
-    }
-
-
-}
-
-$post = new Post();
-
-$post->setTitle("My title");
-$post->setContent("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s");
-//$post->setAuthor($person); objektas setAuthor ir metodas setAuthor vis dar buginti!
-
-
-echo $post->getTitle()."<br>";
-
-echo $post->getAuthor()."<br>";
-
-
-
-//------------------------
-
-
 class Person
 {
 
@@ -83,12 +29,75 @@ private $name = null;
 }
 
 $person = new Person();
-$person->setName("John");
+$person->setName("Ignas");
 $person->setId(10);
 
-echo $person->getName()."<br>";
-echo $person->getId()."<br>";
+echo "My name is: ".$person->getName()."<br>";
+echo "ID is: ".$person->getId()."<br>";
+
+
+class Post
+{
+ private const MAX_LENGHT = 100;
+private $title = null;
+private $content = null;
+private $author = null;
+   
+    
+    //priskiria atitinkamu klases lauku reiksme
+      public function setTitle ($title) {
+    $this->title = $title;
+    }
+     //grazina atitinkamu klases lauku reiksme
+    public function getTitle(){
+         return $this->title;
+    }
+     //priskiria atitinkamu klases lauku reiksme
+    public static function setContent($content){
+        if(strlen($content) > self::MAX_LENGHT) {
+            echo "Max length is: ".self::MAX_LENGHT."<br>";
+        }
+    }
+     //grazina atitinkamu klases lauku reiksme
+       public static function getContent(){ 
+        return self::MAX_LENGHT;
+       }
+     //priskiria atitinkamu klases lauku reiksme
+    public function setAuthor(Person $person){
+        $this->author = $person->getName();
+    }
+     //grazina atitinkamu klases lauku reiksme
+        public function getAuthor(){
+         return $this->author;
+    }
+
+}
+
+
+$post = new Post();
+
+$post->setTitle("My title");
+$post->setContent("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s");
+$post->setAuthor($person);
+
+
+
+echo $post->getTitle()."<br>";
+
+echo $post->getAuthor()."<br>";
+
+
+
+
 
 
 
 ?>
+
+
+
+
+
+
+
+
