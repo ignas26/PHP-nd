33cc43c1baaa151295e967efa4122de2b7fb5414
ignas26 <ignas.zlatkus@gmail.com>
2018-02-24 02:25:14 +0200

    php nd08 finished

--- p08/index.php
+++ p08/index.php
@@ -6,12 +6,13 @@
     $dsn = "mysql:host=$host;dbname=$db";
     $pdo = new PDO($dsn, $user, $password);
     $sql = "
-    SELECT st.student_no, st.surname, st.forename, ms.module_code, ms.mark, md.module_name FROM students st
+    SELECT ms.mark, md.module_name, st.forename, st.surname FROM students st
     LEFT JOIN marks ms
     ON ms.student_no = st.student_no
     LEFT JOIN modules md
     on md.module_code = ms.module_code";
-    $students = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    //sth = $pdo->prepare($sql);
+$students = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     echo '<pre>';
     print_r($students);
     echo '<pre>';
