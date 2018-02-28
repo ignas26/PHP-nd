f33a8335a74c8e3fb93f7248a2bbf79ea67001a4
ignas26 <ignas.zlatkus@gmail.com>
2018-03-01 01:21:12 +0200

    php nd 13 update, give up

--- p13/index.php
+++ p13/index.php
@@ -1,83 +1,25 @@
-<script>
-var a ='https://api.chucknorris.io/jokes/random';
-$.getJSON(a, function (data) {
-$.each(data, function(key, val){
-            $("#data").append(val.category + " ");
-            $("#data").append(val.icon_url + " ");
-            $("#data").append(val.id + " ");
-            $("#data").append(val.url + " ");
-            $("#data").append(val.value + " ");
-  });
-    });
-</script>
-
-<!-- neaisku kaip teisingai issitraukti ir pasikreipti i JSON duomenis -->
-
 <?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db="duombaze";
+    $dsn = "mysql:host=$host;dbname=$db";
+    $pdo = new PDO($dsn, $user, $password);
+    $sql = "
+    INSERT INTO chuckster (category, icon_url, id_varchar, url, value) VALUES (:category, :icon_url, :id_varchar, :url, :value)";
+    $sth = $pdo->prepare($sql);
 
-//$url = "https://api.chucknorris.io/jokes/random";
-//$result = json_decode(file_get_contents($url, true));
-////var_dump($result);
-//
-//foreach ($result as $savybe=>$sav){
-//  echo '<pre>';
-//  var_dump($savybe, $sav);
-//  echo '<pre>';
-//}
+for($i = 0; $i < 5; $i++){
+    
+        $url="https://api.chucknorris.io/jokes/random";
+        $result=json_decode(file_get_contents($url));
 
-
-//
-
-
-
-//class Chuckster {
-//    
-//    public $aaa = null;
-//    public $bbb = null;
-//    public $ccc = null;
-//    public $ddd = null;
-//    public $eee = null;
-//
-//public function __construct($category, $icon, $id, $url, $value){
-//   $this->aaa = $category;
-//   $this->bbb = $icon;
-//   $this->ccc = $id;
-//   $this->ddd = $url;
-//   $this->eee = $value;
-//    }
-// 
-//    
-//public function save(){
-//    $host = "localhost";
-//    $user = "root";
-//    $password = "";
-//    $db = "duombaze";
-//    $dsn = "mysql:host=$host;dbname=$db";
-//    $pdo = new PDO($dsn, $user, $password);
-//
-//
-//         $sql = "
-//    INSERT INTO chuckster (category, icon_url, id_varchar, url, value) VALUES (:category, :icon_url, :id_varchar, :url, :value)";
-//
-//        
-//            $sth = $pdo->prepare($sql);
-//        
-//            $sth->execute([
-//                'category' => $this->category,
-//                'icon_url' => $this->icon,
-//                'id_varchar' => $this->id,
-//                'url' => $this->url,
-//                'value' => $this->value
-//            ]);
-//}
-//}
-//
-//$chuckster = new Chuckster($result{'category'}, $result{'icon_url'}, $result{'id_varchar'}, $result{'url'}, $result{'value'});
-//$chuckster->save();
-
-
-
-?>
-
-
-<script src="jquery-3.2.1.min.js"></script>
+    $sth = $pdo->prepare($sql);
+            $sth->execute(["category" => $result->category,
+                "icon_url" => $result->icon_url,
+                "id_varchar" => $result->id,
+                "url" => $result->url,
+                "value" => $result->value]);
+}
+//pasiduodu, i duombaze isiraso tik kai meta klaida (parefreshinus daugiau kartu, kartais nemeta) ir isiraso tik po viena joke
+    ?>
--- p14/index.php
+++ p14/index.php
@@ -2,25 +2,36 @@
 <?php
 require_once('vendor/autoload.php');
 
+//$faker = Faker\Factory::create();
+//
+//$author = $faker->name; 
+//$authorPost = $faker->text;
+//$time = $faker->date($format = 'Y-m-d', $max = 'now');
+
+
+//function nameGenerator($author, $authorPost, $time){
+//    global $authorPost;
+//    global $author;
+//    global $time;
+    for ($i=0; $i < 1000; $i++) {
+//echo $author, "\n";
+//echo $authorPost, "\n";
+//echo $time, "\n";
 $faker = Faker\Factory::create();
 
 $author = $faker->name; 
 $authorPost = $faker->text;
 $time = $faker->date($format = 'Y-m-d', $max = 'now');
 
-
-function nameGenerator($author, $authorPost, $time){
-//    global $authorPost;
-//    global $author;
-//    global $time;
-    for ($i=0; $i < 1000; $i++) {
-echo $author, "\n";
-echo $authorPost, "\n";
-echo $time, "\n";
+$commented = new Komentaras;
+$commented->author=$faker->name;
+$commented->authorPost=$faker->text;
+$commented->time=$faker->date($format = 'Y-m-d', $max = 'now');
+$commented->save();
 
 }
-}
-nameGenerator($author, $authorPost, $time);
+//}
+//nameGenerator($author, $authorPost, $time);
 
 //nesigauna is ciklo pasiimti rezultato t.y. 1000 irasu (tik viena pagal default).
 
@@ -61,7 +72,7 @@ INSERT INTO comments (author, comment, created_at) VALUES (:author, :comment, :c
 }
 }
 
-$commented = new Komentaras($author, $authorPost, $time);
-$commented->save();
+//$commented = new Komentaras($author, $authorPost, $time);
+//$commented->save();
 
 ?>
