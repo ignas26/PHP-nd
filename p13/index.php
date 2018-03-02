eff505a3ea03ffc38f29559a22812f5660f28d9a
ignas26 <ignas.zlatkus@gmail.com>
2018-03-03 01:29:13 +0200

    php nd 13 finally done but w/o bootstrap therefore ugly appearance

--- p13/index.php
+++ p13/index.php
@@ -1,25 +1,74 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $db="duombaze";
-    $dsn = "mysql:host=$host;dbname=$db";
-    $pdo = new PDO($dsn, $user, $password);
-    $sql = "
-    INSERT INTO chuckster (category, icon_url, id_varchar, url, value) VALUES (:category, :icon_url, :id_varchar, :url, :value)";
-    $sth = $pdo->prepare($sql);
 
-for($i = 0; $i < 5; $i++){
+class Dziaukas 
+{
+    public $icon_url = null;
+    public $id = null;
+    public $url = null;
+    public $joke = null;
+
+function __construct($icon_url, $id, $url, $joke) {
+        
+        $this->icon_url =$icon_url;
+        $this->id =$id;
+        $this->url =$url;
+        $this->joke =$joke;
+    }
+function saveJokes(){
+       
+            $host = "localhost";
+            $user = "root";
+            $password = "";
+            $db = "duombaze";
+            $dsn = "mysql:host=$host;dbname=$db";
+            $pdo = new PDO($dsn, $user, $password);
+           
+$sql = "INSERT INTO chuckster (icon_url, id_varchar, url, value) VALUES (:icon_url, :id_varchar, :url, :value)";
+
+$sth =$pdo->prepare($sql);
+
+    $data = [
+            'icon_url' => $this->icon_url,
+            'id_varchar' => $this->id,
+            'url' => $this->url,
+            'value' => $this->joke ];
+        
+    $sth->execute($data); } }
+
+
+for ($i = 1; $i <= 5; $i++) {
+    $url = "https://api.chucknorris.io/jokes/random";
+    $data = json_decode(file_get_contents($url),true);
+    $jokes = new Dziaukas($data["icon_url"], $data["id"], $data["url"], $data["value"]);
+    $jokes->saveJokes();
+    echo "ikoneles adresas:".$jokes->icon_url."<br><br>";
+    echo "unikalus dziauko identifikacijos nr.:".$jokes->id."<br><br>";
+    echo "dziauko adresas:".$jokes->url."<br><br>";
+    echo "dienos Chuck Norris dziaukas:<h3>".$jokes->joke."</h3><br><br>";}
+
+
     
-        $url="https://api.chucknorris.io/jokes/random";
-        $result=json_decode(file_get_contents($url));
+//zinau, kad kartais islenda "category" masyvas, bandziau patikrinti (su !empty or foreach ciklu), kad apimtu ir ji, bet nepavko, mesdavo error ir i duombaze irasydavo tik viena joke, todel nei objekto aprasyme "category" teko istrinti.
 
-    $sth = $pdo->prepare($sql);
-            $sth->execute(["category" => $result->category,
-                "icon_url" => $result->icon_url,
-                "id_varchar" => $result->id,
-                "url" => $result->url,
-                "value" => $result->value]);
-}
-//pasiduodu, i duombaze isiraso tik kai meta klaida (parefreshinus daugiau kartu, kartais nemeta) ir isiraso tik po viena joke
-    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
